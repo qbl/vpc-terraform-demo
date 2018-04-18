d828c185626b0a1d9afc3abf2c0bc3a8b7da05d5
Iqbal Farabi <farabi.iqbal@gmail.com>
2018-04-18 11:26:18 +0700

    Added configuration for private VPC

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,7 +1,17 @@
+# Public VPC
 resource "aws_vpc" "web-vpc" {
   cidr_block = "10.1.0.0/16"
 
   tags {
     Name = "web-vpc"
+  }
+}
+
+# Private VPC
+resource "aws_vpc" "shared_vpc" {
+  cidr_block = "10.2.0.0/16"
+
+  tags {
+    Name = "shared-vpc"
   }
 }
