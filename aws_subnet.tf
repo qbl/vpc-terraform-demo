94475c8da62482a3db43fc0882159c5c16bc370c
Iqbal Farabi <farabi.iqbal@gmail.com>
2018-04-18 11:26:48 +0700

    Added configuration for private subnet

--- aws_subnet.tf
+++ aws_subnet.tf
@@ -1,6 +1,18 @@
+# Web app public subnet
 resource "aws_subnet" "web-pub" {
   vpc_id = "${aws_vpc.web-vpc.id}"
   cidr_block = "10.1.254.0/24"
+  availability_zone = "ap-southeast-1a"
+
+  tags {
+    Name = "web-pub"
+  }
+}
+
+# Database private subnet
+resource "aws_subnet" "database" {
+  vpc_id = "${aws_vpc.shared-vpc.id}"
+  cidr_block = "10.2.2.0/24"
   availability_zone = "ap-southeast-1a"
 
   tags {
